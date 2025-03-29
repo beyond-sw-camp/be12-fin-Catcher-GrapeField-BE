6a3861dbfa138102374f3f794e444b342b644603
bdt6246 <a01094079599@gmail.com>
2025-03-30 00:42:15 +0900

    [feat]Swagger 추가

--- src/main/java/com/example/demo/base/BaseController.java
+++ src/main/java/com/example/demo/base/BaseController.java
@@ -2,6 +2,7 @@ package com.example.demo.base;
 
 import com.example.demo.base.model.request.BaseReqDto;
 import com.example.demo.base.model.response.BaseRespDto;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -12,15 +13,15 @@ import org.springframework.web.bind.annotation.*;
 public class BaseController {
     private final BaseService baseService;
 
-    //등록
+    @Operation(summary = "등록", description = "등록 기능 기본 양식")
     @PostMapping("/post")
     public ResponseEntity<Long> register(@RequestBody BaseReqDto dto) {
         Long resp = baseService.register(dto);
         return ResponseEntity.ok(resp);
     }
 
-    //단일 조회
     @GetMapping("/get/{idx}")
+    @Operation(summary = "단일 조회", description = "단일 조회 기본 양식")
     public ResponseEntity<BaseRespDto> instance(@RequestParam Long idx){
         BaseRespDto instance = baseService.instance(idx);
         return ResponseEntity.ok(instance);
