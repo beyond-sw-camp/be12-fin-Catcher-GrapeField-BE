c43cd21bf2eeed457c440b9f8c127edde6f0e0fd
bdt6246 <a01094079599@gmail.com>
2025-04-17 18:35:25 +0900

    [fix] 기능 분리 앞으로 구현해야할 기능 표시 및 구현 위치 이동

--- src/main/java/com/example/grapefield/user/customercenter/CustomerCenterController.java
+++ src/main/java/com/example/grapefield/user/customercenter/CustomerCenterController.java
@@ -12,4 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/customercenter")
 @Tag(name="8. 고객센터", description = "공지사항, FAQ, QnA 등 고객 편의를 다루는 기능")
 public class CustomerCenterController {
+
+
+  //TODO: 공지사항, Qna, FaQ,
 }
