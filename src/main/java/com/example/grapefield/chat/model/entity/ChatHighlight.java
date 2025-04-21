70605dee0b3a563075b5af1d59d420f95e096628
Hyorim K <37091594+daydeiday@users.noreply.github.com>
2025-04-21 17:24:11 +0900

    [refactor] ChatHighlight 제약조건 및 페치조인 설정 추가

--- src/main/java/com/example/grapefield/chat/model/entity/ChatHighlight.java
+++ src/main/java/com/example/grapefield/chat/model/entity/ChatHighlight.java
@@ -27,7 +27,16 @@ public class ChatHighlight {
     private String description;
     private Long messageCnt;
 
-    @OneToOne
-    @JoinColumn(name = "message_idx", unique = true)
+    /*
+    optional = false
+    - JPA 수준에서 "반드시 값이 있어야 한다"고 선언
+    fetch = FetchType.LAZY
+    - 불필요한 조인 없이 필요할 때만 메세지를 로딩하도록 함
+
+    @JoinColumn(nullable = false)
+    - 데이터베이스 컬럼에도 NOT NULL 제약을 걸어 삽입 시점에 오류가 나도록 강제
+    * */
+    @OneToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "message_idx", unique = true, nullable = false)
     private ChatMessageBase message;
 }
