f7bde80cc388acbff0d71d52ca9ad130723838fa
bdt6246 <a01094079599@gmail.com>
2025-04-25 18:44:16 +0900

    [fix] actuator 설정 추가

--- src/main/java/com/example/grapefield/config/SecurityConfig.java
+++ src/main/java/com/example/grapefield/config/SecurityConfig.java
@@ -96,7 +96,7 @@ public class SecurityConfig {
     http.authorizeHttpRequests(authorizeRequests -> {
       authorizeRequests
           // 인증 없이 접근 가능한 경로
-          .requestMatchers("/user/signup", "/login", "/logout", "/user/email_verify", "/user/email_verify/**", "/events/**", "/participant/**", "/chat/**", "/chat/list/**", "/post/list/**", "/post/**", "/comment/**", "/review/**", "/auth/**", "/search/**").permitAll()
+          .requestMatchers("/user/signup", "/login", "/logout", "/user/email_verify", "/user/email_verify/**", "/events/**", "/participant/**", "/chat/**", "/chat/list/**", "/post/list/**", "/post/**", "/comment/**", "/review/**", "/auth/**", "/search/**","/actuator/**").permitAll()
           .requestMatchers("/swagger-ui.html", "/swagger-ui/**", "/v3/api-docs/**",
               "/v3/api-docs", "/swagger-resources/**", "/webjars/**").permitAll()
          // 관리자 권한 필요
