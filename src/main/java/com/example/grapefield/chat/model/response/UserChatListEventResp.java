2644f23a303071f658b5b680df26dc0764419597
s00ya <jjschoho@gmail.com>
2025-05-12 22:20:05 +0900

    feat: 사이드바 채팅방 목록 실시간 반영 추가 #184

--- src/main/java/com/example/grapefield/chat/model/response/UserChatListEventResp.java
+++ src/main/java/com/example/grapefield/chat/model/response/UserChatListEventResp.java
@@ -1,4 +1,14 @@
 package com.example.grapefield.chat.model.response;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class UserChatListEventResp {
+    private String type;
+    private Long userIdx;
+    private Long roomIdx;
+    private String action;
+    private Long timestamp;
 }
