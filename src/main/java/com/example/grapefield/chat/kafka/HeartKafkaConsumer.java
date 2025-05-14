d18f306f84ccf7aac8093d5d71f66953ab17fa8a
Hyorim K <37091594+daydeiday@users.noreply.github.com>
2025-05-14 16:21:55 +0900

    [fix] 오타 수정

--- src/main/java/com/example/grapefield/chat/kafka/HeartKafkaConsumer.java
+++ src/main/java/com/example/grapefield/chat/kafka/HeartKafkaConsumer.java
@@ -35,14 +35,18 @@ public class HeartKafkaConsumer {
         // 기존에 없는 경우 추가 되지 않기 때문에 때문에 added에 0을 반환한다.
         Long added = redisTemplate.opsForSet().add("processed:hearts", heartIdx);
         Long newCount;
-        if(added != null && added > 0) {
+        if(added != null && added == 0) {
             newCount=chatRoomService.increaseHeart(roomIdx);
         } else {
             log.info("이미 처리된 요청이므로 취소");
             String redisKey = "chat:"+roomIdx+":likes";
             newCount = Long.parseLong((redisTemplate.opsForValue().get(redisKey)).toString());
         }
-        // 2. WebSocket 브로커로 브로드캐스트 (프론트에서 애니메이션 띄우게)
+
+        //
+
+
+        // WebSocket 브로커로 브로드캐스트 (프론트에서 애니메이션 띄우게)
         HeartResp resp = new HeartResp(roomIdx, newCount);
         // // Redis 없는 환경에서는 다음으로 바꾸면 된다.⤵️⤵️
         // HeartResp resp = new HeartResp(roomIdx, chatRoom.getHeartCnt());
